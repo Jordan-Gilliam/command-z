f04aa63b4616b99a3e3d6ce4c6ae4778b51e8037
Jordan Gilliam <gilliamja.te@gmail.com>
2019-10-25 22:53:33 -0400

    Fixed login script

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ const { user, pass, name } = require('./env');
   try {
     await goToUrl(page, 'login');
     await waitForEl(page, 'div:nth-child(2) > input');
-    await typeInEl(page, 'div:nth-child(2) input', user);
-    await waitForEl(page, 'div:nth-child(3) > input');
-    await typeInEl(page, 'div:nth-child(3) input', pass);
+    await typeInEl(page, 'div:nth-child(2) > input', user);
+    await typeInEl(page, 'fieldset > div:nth-child(3) > input', pass);
     await click(page, 'form div.clearfix button');
     await goToUrl(page, `${name}/with_replies`);
   } catch (e) {
--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 const goToUrl = async (page, u) => page.goto(`https://twitter.com/${u}`, { waitUntil: 'networkidle0' });
-const waitForEl = async (page, e) => page.waitForSelector(e);
-const typeInEl = async (page, e, v) => page.type(e, v);
-const click = async (page, e) => page.click(e);
+const waitForEl = async (page, el) => page.waitForSelector(el);
+const typeInEl = async (page, el, val) => page.type(el, val);
+const click = async (page, el) => page.click(el);
 
 module.exports = { goToUrl, waitForEl, typeInEl, click };
